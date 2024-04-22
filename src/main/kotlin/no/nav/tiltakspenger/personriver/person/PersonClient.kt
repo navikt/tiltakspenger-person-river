e23d3ba1017ad18f478f5995ca34eb1407de0766
Sneha Desai <sneha.desai@gmail.com>
2024-04-22 06:32:20 +0200

    La til feilhåndtering til hentPerson i PersonClient

--- src/main/kotlin/no/nav/tiltakspenger/personriver/person/PersonClient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/personriver/person/PersonClient.kt
@@ -1,12 +1,14 @@
 package no.nav.tiltakspenger.personriver.person
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.accept
 import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.header
 import io.ktor.client.request.prepareGet
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import no.nav.tiltakspenger.libs.person.PersonRespons
 import no.nav.tiltakspenger.personriver.httpClientCIO
@@ -26,16 +28,19 @@ class PersonClient(
     )
 
     suspend fun hentPerson(ident: String): PersonRespons {
-        val httpResponse: PersonRespons =
+        val httpResponse =
             httpClient.prepareGet("${personKlientConfig.baseUrl}/azure/pdl/personalia") {
                 header(navCallIdHeader, "tiltakspenger-person-river")
                 bearerAuth(getToken())
                 accept(ContentType.Application.Json)
                 contentType(ContentType.Application.Json)
                 setBody(RequestBody(ident))
-            }.body()
+            }.execute()
 
-        return httpResponse
+        return when (httpResponse.status) {
+            HttpStatusCode.OK -> httpResponse.call.response.body<PersonRespons>()
+            else -> throw RuntimeException("error (responseCode=${httpResponse.status.value}) from tiltakspenger-person")
+        }
     }
 
     data class PersonKlientConfig(
